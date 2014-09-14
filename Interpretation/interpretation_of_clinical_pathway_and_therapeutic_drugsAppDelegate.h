289f2f98efd0fbce037f6ba6757ff7b55447015a
zhihuiyun <zhihuiyun1@zhihuiyun1deMacBook-Air.local>
2014-09-14 08:32:38 +0800

    Initial Commit

---
+++ Interpretation/interpretation_of_clinical_pathway_and_therapeutic_drugsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  interpretation_of_clinical_pathway_and_therapeutic_drugsAppDelegate.h
+//  Interpretation
+//
+//  Created by zhihuiyun on 14-9-14.
+//  Copyright (c) 2014年 zhiHuiYun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface interpretation_of_clinical_pathway_and_therapeutic_drugsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Interpretation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Interpretation
+//
+//  Created by zhihuiyun on 14-9-14.
+//  Copyright (c) 2014年 zhiHuiYun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "interpretation_of_clinical_pathway_and_therapeutic_drugsAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([interpretation_of_clinical_pathway_and_therapeutic_drugsAppDelegate class]));
+    }
+}
